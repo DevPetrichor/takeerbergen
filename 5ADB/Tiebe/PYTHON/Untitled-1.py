aec6c4c274fbe47954c014601706b535979cc3d8
tiebevandenmuisenberg-a11y <tiebe.vandenmuisenberg@gmail.com>
2026-01-14 11:12:09 +0000

    tiebe eindelijk erin

---
+++ 5ADB/Tiebe/PYTHON/Untitled-1.py
@@ -0,0 +1,6 @@
+getal = int(input("Geef een getal: "))
+#dit vraagt de gebruiker om een getal in te vullen
+uitkomst = getal * 2
+#dit verdubbelt het getal
+print("Het dubbele van je getal is", uitkomst, ".")
+#dit print het resultaat van de verdubbeling 
