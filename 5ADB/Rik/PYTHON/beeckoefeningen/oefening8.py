54becf58c94ebd2c571f7da3cb675887b2493238
Petrichor <102480092+DevPetrichor@users.noreply.github.com>
2026-01-14 10:43:46 +0000

    python oefening 8.py

--- 5ADB/Rik/PYTHON/beeckoefeningen/oefening8.py
+++ 5ADB/Rik/PYTHON/beeckoefeningen/oefening8.py
@@ -1,10 +1,15 @@
-fruit = {"appel": 2.5, "banaan": 1.80, "peer": 2.20}
-fruit = str(input("Welk fruit wil je kopen? "))
-if fruit in fruit:
+fruit = {"appel": 2.5, "banaan": 1.80, "peer": 2.20} # prijzen per kg
+keuze = str(input("Welk fruit wil je kopen? "))
+if keuze in fruit: # controleer of het fruit in de lijst staat
     gewicht = float(input("Hoeveel kg wil je kopen? "))
-    prijskg = fruit[fruit]
-    totaalprijs = gewicht * prijskg
-    print(f"De totaalprijs voor {gewicht} kg {fruit} is {totaalprijs} euro.")
+    prijskg = fruit[keuze] # haal de prijs per kg op
+    totaalprijs = gewicht * prijskg # bereken de totaalprijs
+    if gewicht >= 5: # als het gewicht 5 kg of meer is
+        korting = totaalprijs * 0.1 # pas 10% korting toe bij 5 kg of meer
+    print(f"De totaalprijs voor {gewicht} kg {keuze} is {totaalprijs} euro.")
+    if gewicht >= 5:
+        print(f"Aangezien u {gewicht} kg {keuze} kocht, heeft u een korting van {korting} euro gekregen.")
+        print(f"De te betalen prijs is {totaalprijs - korting} euro.")
 else:
     print("Sorry, dat fruit hebben we niet.")
 
