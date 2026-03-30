13dde63a69c5adf23f12424ae5c56effc050b842
tiebevandenmuisenberg-a11y <tiebe.vandenmuisenberg@gmail.com>
2026-03-30 08:49:17 +0000

    les

---
+++ 5ADB/Tiebe/PYTHON/30-3.py
@@ -0,0 +1,24 @@
+keuze=int(input("""Kies een optie:
+ oefening 1:Opgave herhaling for and while
+ oefening 2:Opgave voorschot berekenen
+ oefening 3: Opgave gewicht omzetten
+ """))
+if keuze==1:
+    a=1
+    for i in range(1,6):
+        print(i)
+        
+    while a< 6:
+        print(a)
+        a += 1
+
+if keuze==2:
+    prijsHuis = float(input("Wat is de prijs van het huis? "))
+    WaardeBankRekening = float(input("Wat is de waarde van je bankrekening? "))
+    if WaardeBankRekening >= prijsHuis/4:
+        if WaardeBankRekening > prijsHuis:
+            print(f"Je moet 10% van het bedrag betalen wat neerkomt op {prijsHuis*0.10} euro")
+        else:
+            print(f"Je moet 20% van het bedrag betalen wat neerkomt op {prijsHuis*0.20} euro")
+    else:
+        print("Je hebt niet genoeg geld om het huis te kopen!")
