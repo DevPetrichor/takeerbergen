51c1f4cb19fcb789a7f552db416d62229c67ed4b
Petrichor <102480092+DevPetrichor@users.noreply.github.com>
2026-06-01 09:51:16 +0200

    oef10

--- 5ADB/Rik/PYTHON/beeckoefeningen/oefening10.py
+++ 5ADB/Rik/PYTHON/beeckoefeningen/oefening10.py
@@ -1,72 +1,45 @@
-def ElectrischeAuto():
-    aankoopPrijs = 31895 #  whattherange.com/elektrische-auto - Volvo EX30
-    onderhoud = 350 # Volvo Care Offer
-    biv = 61.5 # belastingen.fenb.be - Simulatie
-    verzekering = 1170 # verzekeringen.be - Yuzzu Full Omnium
-    brandstof = 0.3306 # engie.be - Blog
-    kwh = 51 # whattherange.com/elektrische-auto - Volvo EX30
-    batterij = 254 # whattherange.com/elektrische-auto - Volvo EX30
-    kilometers = 15000
-    verkeersBelasting = 102.96 # belastingen.fenb.be - Simulatie
-    totaalJaar = 17
+class auto:
+    def __init__(self, aankoopPrijs, onderhoud, biv, verzekering, brandstof, kilometers, verkeersBelasting):
+        self.aankoopPrijs = aankoopPrijs
+        self.onderhoud = onderhoud
+        self.biv = biv
+        self.verzekering = verzekering
+        self.brandstof = brandstof
+        self.kilometers = kilometers
+        self.verkeersBelasting = verkeersBelasting
+    def energiePerJaar(self):
+        return self.brandstof * (self.kilometers / 15)
+    def totaalKM(self, totaalJaar):
+        return self.kilometers * totaalJaar
+    def prijsEersteJaar(self, totaalJaar):
+        return self.aankoopPrijs + self.onderhoud + self.biv + self.verzekering + self.energiePerJaar() + self.verkeersBelasting
+    def prijsVervolgJaar(self, totaalJaar):
+        return self.onderhoud + self.verzekering + self.energiePerJaar() + self.verkeersBelasting
+    def totaalPrijs(self, totaalJaar):
+        return self.prijsEersteJaar(totaalJaar) + self.prijsVervolgJaar(totaalJaar) * (totaalJaar - 1)
+    def prijsPerKM(self, totaalJaar):
+        return self.totaalPrijs(totaalJaar) / self.totaalKM(totaalJaar)
+    def printPrijs(self, totaalJaar):
+        for jaar in range(1, totaalJaar + 1):
+            if jaar == 1:
+                print(f"Jaar {jaar}: Prijs = {self.prijsEersteJaar(totaalJaar):.2f}")
+            else:
+                uitgerekend = self.prijsVervolgJaar(totaalJaar) + self.prijsVervolgJaar(totaalJaar) * (jaar - 1)
+                print(f"Jaar {jaar}: Prijs = {uitgerekend:.2f}")
+        print("De totale prijs van de auto over", totaalJaar, "jaar is:", f"{self.totaalPrijs(totaalJaar):.2f}")
+        print("De prijs per jaar is:", f"{self.totaalPrijs(totaalJaar) / totaalJaar:.2f}")
+        print("De prijs per kilometer is:", f"{self.prijsPerKM(totaalJaar):.2f}")
+        print(f"Prijs eerste jaar: {self.prijsEersteJaar(totaalJaar):.0f}")
+        print(f"Prijs vervolgjaar: {self.prijsVervolgJaar(totaalJaar):.0f}")
+    def __str__(self):
+        return f"Aankoopprijs: {self.aankoopPrijs}, Onderhoud: {self.onderhoud}, BIV: {self.biv}, Verzekering: {self.verzekering}, Brandstof: {self.brandstof}, Kilometers: {self.kilometers}, Verkeersbelasting: {self.verkeersBelasting}"
 
-    energiePerJaar = brandstof * kwh * (kilometers / batterij)
 
-    totaalKM = kilometers * totaalJaar
-
-    prijsEersteJaar = aankoopPrijs + onderhoud + biv + verzekering + energiePerJaar + verkeersBelasting
-    prijsVervolgJaar = onderhoud + verzekering + energiePerJaar + verkeersBelasting
-
-    for jaar in range(1, totaalJaar + 1):
-        if jaar == 1:
-            print(f"Jaar {jaar}: Prijs = {prijsEersteJaar:.2f}")
-        else:
-            uitgerekend = prijsVervolgJaar + prijsVervolgJaar * (jaar - 1)
-            print(f"Jaar {jaar}: Prijs = {uitgerekend:.2f}")
-
-    totaalPrijs = prijsEersteJaar + prijsVervolgJaar * (totaalJaar - 1)
-    prijsPerKM = totaalPrijs / totaalKM
-
-    print("De totale prijs van de auto over", totaalJaar, "jaar is:", f"{totaalPrijs:.2f}")
-    print("De prijs per jaar is:", f"{totaalPrijs / totaalJaar:.2f}")
-    print("De prijs per kilometer is:", f"{prijsPerKM:.2f}")
-    print(f"Prijs eerste jaar: {prijsEersteJaar:.0f}")
-    print(f"Prijs vervolgjaar: {prijsVervolgJaar:.0f}")
-
-def BenzineAuto():
-    aankoopPrijs = 31.499 # cardoen.be - Kia ProCeed
-    onderhoud = 804 # cardoen.be - onderhoudscontract
-    biv = 0 # belastingen.fenb.be - Simulatie
-    verzekering = 1.248 # cardoen.be - autoverzekering
-    brandstof = 1.861 # anwb.nl - Benzineprijs
-    kilometers = 15000
-    verkeersBelasting =   250.4 # belastingen.fenb.be - Simulatie
-    totaalJaar = 17
-
-    energiePerJaar = brandstof * (kilometers / 15) 
-
-    totaalKM = kilometers * totaalJaar
-
-    prijsEersteJaar = aankoopPrijs + onderhoud + biv + verzekering + energiePerJaar + verkeersBelasting
-    prijsVervolgJaar = onderhoud + verzekering + energiePerJaar + verkeersBelasting
-
-    for jaar in range(1, totaalJaar + 1):
-        if jaar == 1:
-            print(f"Jaar {jaar}: Prijs = {prijsEersteJaar:.2f}")
-        else:
-            uitgerekend = prijsVervolgJaar + prijsVervolgJaar * (jaar - 1)
-            print(f"Jaar {jaar}: Prijs = {uitgerekend:.2f}")
-
-    totaalPrijs = prijsEersteJaar + prijsVervolgJaar * (totaalJaar - 1)
-    prijsPerKM = totaalPrijs / totaalKM
-
-    print("De totale prijs van de auto over", totaalJaar, "jaar is:", f"{totaalPrijs:.2f}")
-    print("De prijs per jaar is:", f"{totaalPrijs / totaalJaar:.2f}")
-    print("De prijs per kilometer is:", f"{prijsPerKM:.2f}")
-    print(f"Prijs eerste jaar: {prijsEersteJaar:.0f}")
-    print(f"Prijs vervolgjaar: {prijsVervolgJaar:.0f}")
+# aankoopPrijs, onderhoud, biv, verzekering, brandstof, kilometers, verkeersBelasting
+elektrischeAuto = auto(31895, 350, 61.5, 1170, 0.3306, 15000, 102.96)
+benzineAuto = auto(31.499, 804, 0, 1.248, 1.861, 15000, 250.4)
 
 print("Elektrische Auto:")
-ElectrischeAuto()
+elektrischeAuto.printPrijs(17)
 print("\nBenzine Auto:")
-BenzineAuto()
+benzineAuto.printPrijs(17)
